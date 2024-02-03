68702c9956f2ed803fe05f567d36abdb907f545b
Aldo Rosete <aldorc78@gmail.com>
2024-02-02 18:03:37 -0600

    Adding comments to the Playlist code

--- DesertIslandPlaylist/DesertIslandPlaylist.java
+++ DesertIslandPlaylist/DesertIslandPlaylist.java
@@ -5,8 +5,10 @@ class DesertIslandPLaylist
   
   public static void main(String[] args) 
   {
+    //Creating a new  String Arraylist 
     ArrayList<String> desertIslandPlaylist = new ArrayList<String>(); 
 
+    //Adding items to the playlist (Songs)
     desertIslandPlaylist.add("Always-Bon Jovi");
     desertIslandPlaylist.add("Bad Medicine-Bon Jovi");
     desertIslandPlaylist.add("First Date-Blink-182");
@@ -18,16 +20,15 @@ class DesertIslandPLaylist
 
     System.out.println("\n I added "+desertIslandPlaylist.size()+ " songs.");
 
-    desertIslandPlaylist.remove(0);
-    System.out.println("\nAfter removing now i have "+desertIslandPlaylist.size()+ " songs.");
+    
+    desertIslandPlaylist.remove(0); //It turns out I can only bring 5 songs to the Island so i deleted the first song
+    System.out.println("\nAfter removing now i have "+desertIslandPlaylist.size()+ " songs."); //Cheking number of songs in playlist
 
-     //After this line im gonna change the order of two songs. 
-
+     //After this line im gonna change the order of two songs.  
      int a = desertIslandPlaylist.indexOf("Poppin' Champagne-All Time Low");
-
      int b = desertIslandPlaylist.indexOf("Bad Medicine-Bon Jovi");
 
-     
+    //Changing position with "Set" command
     desertIslandPlaylist.set(a,"Bad Medicine-Bon Jovi");
     desertIslandPlaylist.set(b,"Poppin' Champagne-All Time Low");
     System.out.println(desertIslandPlaylist);
